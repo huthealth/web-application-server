b177958eeae48f6dde2c68b2cff531c34fe602fb
leechangkuk <huthealth@naver.com>
2020-06-23 10:58:42 +0900

    요구사항2&3 완료

--- src/main/java/webserver/RequestHandler.java
+++ src/main/java/webserver/RequestHandler.java
@@ -3,8 +3,10 @@ package webserver;
 import java.io.*;
 import java.net.Socket;
 import java.nio.file.Paths;
+import java.util.Map;
 
 import com.google.common.io.Files;
+import model.User;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import util.IOUtils;
@@ -26,34 +28,63 @@ public class RequestHandler extends Thread {
             // TODO 사용자 요청에 대한 처리는 이 곳에 구현하면 된다.
             DataOutputStream dos = new DataOutputStream(out);
             BufferedReader br = new BufferedReader(new InputStreamReader(in));
-            String[] request = requestParser(br);
+            String request = requestParser(br);
 
-            byte[] body = getHtmlFile(request[0]);
+            byte[] body = getHtmlFile(request);
 
-            response200Header(dos,request[1] ,body.length);
+            response200Header(dos ,body.length);
             responseBody(dos, body);
         } catch (IOException e) {
             log.error(e.getMessage());
         }
     }
 
-    private String[] requestParser(BufferedReader br) throws IOException {
-        String[] ret = new String[2];
+    private String requestParser(BufferedReader br) throws IOException {
+
         String header = br.readLine();
         String[] token = header.split(" ");
-        ret[0] = token[1];
+        String method = token[0];
+        String url = token[1];
+        int contextLen = 0;
+
         while(!"".equals(header)) {
             if(header == null) break;
             System.out.println(header);
             token = header.split(" ");
-            if(token[0].equals("Accept:")) {
-                String[] content = token[1].split(",");
-                ret[1] = content[0];
-            }
+            if(token[0].equals("Content-Length:")) contextLen = Integer.parseInt(token[1]);
             header = br.readLine();
         }
-        System.out.println(ret[0] + " " + ret[1]);
-        return ret;
+
+
+        if(url.length() >11  && url.substring(0,12).matches("/user/create")) {
+            if(method.equals("POST")) {
+                //br.readLine();
+                String httpBody = util.IOUtils.readData(br,contextLen);
+                createUser(httpBody);
+            }
+            else {
+                String[] temp = url.split("\\?");
+                url = temp[0];
+                createUser(temp[1]);
+            }
+        }
+        return url;
+    }
+
+    private boolean createUser(String param) {
+        Map<String,String> paramMap = util.HttpRequestUtils.parseQueryString(param);
+        String userId = paramMap.get("userId");
+        String password = paramMap.get("password");
+        String name = paramMap.get("name");
+        String email = paramMap.get("email");
+
+        if(userId == null || password == null || name == null || email == null) {
+            System.out.println("회원가입 실패");
+            return false;
+        }
+        User newUser = new User(userId,password,name,email);
+        System.out.println("회원가입 성공");
+        return true;
     }
 
     private byte[] getHtmlFile(String url ) throws IOException {
@@ -69,11 +100,11 @@ public class RequestHandler extends Thread {
         return java.nio.file.Files.readAllBytes(file.toPath());
     }
 
-    private void response200Header(DataOutputStream dos, String type, int lengthOfBodyContent) {
+    private void response200Header(DataOutputStream dos, int lengthOfBodyContent) {
         try {
 
             dos.writeBytes("HTTP/1.1 200 OK \r\n");
-            dos.writeBytes("Content-Type:"+type+"\r\n");
+            dos.writeBytes("Content-Type:text/html\r\n");
             dos.writeBytes("Content-Length: " + lengthOfBodyContent + "\r\n");
             dos.writeBytes("\r\n");
         } catch (IOException e) {
