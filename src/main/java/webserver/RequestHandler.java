567e70ef189955ca7f00fed57755c6c72ab43dce
leechangkuk <huthealth@naver.com>
2020-06-28 17:54:02 +0900

    Extract Method 리팩토링 run() 메소드의 복잡도가 높아 각 분기문 구현을 별도의 메소드로 분리

--- src/main/java/webserver/RequestHandler.java
+++ src/main/java/webserver/RequestHandler.java
@@ -17,10 +17,6 @@ public class RequestHandler extends Thread {
 
     private Socket connection;
 
-    private HttpRequest request;
-    private HttpResponse response;
-
-
     public RequestHandler(Socket connectionSocket) {
         this.connection = connectionSocket;
     }
@@ -32,79 +28,87 @@ public class RequestHandler extends Thread {
         try (InputStream in = connection.getInputStream(); OutputStream out = connection.getOutputStream()) {
             // TODO 사용자 요청에 대한 처리는 이 곳에 구현하면 된다.
 
-            request = new HttpRequest(in);
-            response = new HttpResponse(out);
+            HttpRequest request = new HttpRequest(in);
+            HttpResponse response = new HttpResponse(out);
 
            String url = request.getPath();
-           String method = request.getMethod();
-           String body = request.getBody();
 
-            if(url.equals("/user/create") ) {
-                User user = new User(request.getParameter("userId"),request.getParameter("password"),
-                        request.getParameter("name"),request.getParameter("email"));
-                DataBase.addUser(user);
-                String location = "/index.html";
-                response.sendRedirect(location);
+            if("/user/create".equals(url) ) {
+                createUser(request, response);
             }
 
-            else if (url.equals("/user/login")) {
-                boolean foundUser = false;
-                String userId = request.getParameter("userId");
-                String password = request.getParameter("password");
-                User loginUser = DataBase.findUserById(userId);
-                if(loginUser != null && loginUser.getPassword().equals(password)) {
-                    response.addHeader("Set-Cookie", "logined=true");
-                    response.sendRedirect("/index.html");
-                }
-                else {
-                    response.addHeader("Set-Cookie", "logined=false");
-                    response.sendRedirect("/user/login_failed.html");
-                }
+            else if ("/user/login".equals(url)) {
+                login(request, response);
             }
 
-            else if(url.equals("/user/list")) {
-                String cookie = request.getHeader("Cookie");
-                Map<String, String> cookieMap = util.HttpRequestUtils.parseCookies(cookie);
-                String isLogin = cookieMap.get("logined");
-                if (isLogin != null) {
-                    if (isLogin.equals("true")) {
-                        StringBuilder sb = new StringBuilder();
-                        Collection<User> userCollection = DataBase.findAll();
-                        sb.append(
-                                "<!DOCTYPE html>\n" +
-                                        "<html lang=\"en\">" +
-                                        "<TABLE BORDER=1>\n" +
-                                        "<CAPTION>User List</CAPTION>\n" +
-                                        "<TR>\n" +
-                                        "    <TD>ID</TD>\n" +
-                                        "    <TD>NAME</TD>\n" +
-                                        "    <TD>EMAIL</TD>\n" +
-                                        "</TR>");
-                        for (User user : userCollection) {
-                            sb.append("<TR>\n +" +
-                                    "    <TD>" + user.getUserId() + "</TD>\n +" +
-                                    "    <TD>" + user.getName() + "</TD>\n +" +
-                                    "    <TD>" + user.getEmail() + "</TD>\n +" +
-                                    "<TR>");
-                        }
-                        sb.append("</TABLE></html>");
-                        String table = sb.toString();
-                        response.forwardBody(table);
-                    }
-                }
-                else {
-                    response.sendRedirect("/user/login.html");
-                }
+            else if("/user/list".equals(url)) {
+                listUser(request, response);
             }
-
             else {
                 response.forward(url);
             }
         }
-
         catch (IOException e) {
             log.error(e.getMessage());
         }
+    }
+
+    private void listUser(HttpRequest request, HttpResponse response) {
+        String cookie = request.getHeader("Cookie");
+        Map<String, String> cookieMap = util.HttpRequestUtils.parseCookies(cookie);
+        String isLogin = cookieMap.get("logined");
+        if (isLogin != null) {
+            if (isLogin.equals("true")) {
+                StringBuilder sb = new StringBuilder();
+                Collection<User> userCollection = DataBase.findAll();
+                sb.append(
+                        "<!DOCTYPE html>\n" +
+                                "<html lang=\"en\">" +
+                                "<TABLE BORDER=1>\n" +
+                                "<CAPTION>User List</CAPTION>\n" +
+                                "<TR>\n" +
+                                "    <TD>ID</TD>\n" +
+                                "    <TD>NAME</TD>\n" +
+                                "    <TD>EMAIL</TD>\n" +
+                                "</TR>");
+                for (User user : userCollection) {
+                    sb.append("<TR>\n +" +
+                            "    <TD>" + user.getUserId() + "</TD>\n +" +
+                            "    <TD>" + user.getName() + "</TD>\n +" +
+                            "    <TD>" + user.getEmail() + "</TD>\n +" +
+                            "<TR>");
+                }
+                sb.append("</TABLE></html>");
+                String table = sb.toString();
+                response.forwardBody(table);
+            }
+        }
+        else {
+            response.sendRedirect("/user/login.html");
+        }
+    }
+
+    private void login(HttpRequest request, HttpResponse response) {
+        boolean foundUser = false;
+        String userId = request.getParameter("userId");
+        String password = request.getParameter("password");
+        User loginUser = DataBase.findUserById(userId);
+        if(loginUser != null && loginUser.getPassword().equals(password)) {
+            response.addHeader("Set-Cookie", "logined=true");
+            response.sendRedirect("/index.html");
+        }
+        else {
+            response.addHeader("Set-Cookie", "logined=false");
+            response.sendRedirect("/user/login_failed.html");
+        }
+    }
+
+    private void createUser(HttpRequest request, HttpResponse response) {
+        User user = new User(request.getParameter("userId"),request.getParameter("password"),
+                request.getParameter("name"),request.getParameter("email"));
+        DataBase.addUser(user);
+        String location = "/index.html";
+        response.sendRedirect(location);
     }
 
 }
